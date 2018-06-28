8d5b0b30d2e178426289e4ca2ba20ca2499ca3ba
osman144 <osman144@umn.edu>
2018-06-28 17:33:05 -0500

    nav wrapper

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Nav from "./components/Nav";
-import GameInstructions from "./components/Game Instructions";
-import friends from "friends.json";
+// import GameInstructions from "./components/GameInstructions";
+import friends from "./friends.json";
 import './App.css';
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends: friends
   };
 
     // Map over this.state.friends and render a FriendCard component for each friend object
+  render (){
+    return (
+      <Wrapper>
+        <Nav>Friend's List</Nav>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            // key={friend.id}
+            image={friend.image}
+          
+          />
+        ))}
+      </Wrapper>
 
+    )
+
+  }
 
 
 
